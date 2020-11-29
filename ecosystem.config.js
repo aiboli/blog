db22706450176e093700a9957452f9addeb80a34
Aibo Li <aibli@microsoft.com>
2020-11-29 13:45:01 -0800

    update config file

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+      {
+        name: 'blog',
+        cwd: '/srv/strapi/blog',
+        script: 'npm',
+        args: 'start',
+        env: {
+        },
+      },
+    ],
+  };
